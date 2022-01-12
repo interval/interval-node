9a7116965095e2210b068f56b411d4c53b583c5e
Alex Arena <arena.secure@gmail.com>
2022-01-11 19:11:08 -0800

    more wip toward a "production" app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import { WebSocket } from "ws";
 interface InternalConfig {
   name: string;
 }
 
 export default function createIntervalHost(config: InternalConfig) {
+  console.log("Create internal app!");
+  const ws = new WebSocket("ws://localhost:2023");
+  ws.onclose = () => console.log("closed");
+  ws.onopen = async () => {
+    console.log("Connected, making caller!");
+  };
+
   return true;
 }
