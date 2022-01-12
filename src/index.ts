f0311912e1f12690f5a76a9d1f68680e658ea8cd
Jacob Mischka <jacob@mischka.me>
2022-01-12 03:20:16 -0600

    Change prettier config and apply globally

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { WebSocket } from "ws";
+import { WebSocket } from 'ws'
 interface InternalConfig {
-  name: string;
+  name: string
 }
 
 export default function createIntervalHost(config: InternalConfig) {
-  console.log("Create internal app!");
-  const ws = new WebSocket("ws://localhost:2023");
-  ws.onclose = () => console.log("closed");
+  console.log('Create internal app!')
+  const ws = new WebSocket('ws://localhost:2023')
+  ws.onclose = () => console.log('closed')
   ws.onopen = async () => {
-    console.log("Connected, making caller!");
-  };
+    console.log('Connected, making caller!')
+  }
 
-  return true;
+  return true
 }
