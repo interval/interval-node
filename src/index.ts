ec371ce4f489aae61c83f477ced8920f5e1738d0
Alex Arena <arena.secure@gmail.com>
2022-01-12 19:31:48 -0800

    working RPC!

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import { WebSocket } from "ws";
+import ISocket from "../../common/ISocket";
+import { createCaller } from "../../common/rpc";
+import { internalRpcSchema } from "../../common/internalRpcSchema";
+
 interface InternalConfig {
-  name: string;
+  apiKey: string;
 }
 
-export default function createIntervalHost(config: InternalConfig) {
-  console.log("Create internal app!");
-  const ws = new WebSocket("ws://localhost:2023");
-  ws.onclose = () => console.log("closed");
-  ws.onopen = async () => {
-    console.log("Connected, making caller!");
-  };
+export default async function createIntervalHost(config: InternalConfig) {
+  console.log("Create Interval Host :)", config);
+  const ws = new ISocket(new WebSocket("ws://localhost:2023"), {
+    timeout: 3000,
+  });
+
+  await ws.connect();
+
+  const caller = createCaller({
+    schema: internalRpcSchema,
+    send: (rawInput) => ws.send(rawInput),
+  });
+  ws.on("message", (m) => caller.replyHandler(m));
+
+  const lo = await caller.client("LOGIN", { apiKey: config.apiKey });
+  console.log("Login response:", lo);
 
   return true;
 }
