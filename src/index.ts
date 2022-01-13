712c351099ff491f23bf1b82d7f152a9b6257474
Alex Arena <arena.secure@gmail.com>
2022-01-12 22:58:03 -0800

    wip reconnect/error handling

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,64 @@ interface InternalConfig {
   apiKey: string;
 }
 
+function autoTry(fn: () => any) {
+  const steps = [1000, 3000, 10000];
+  let triesAtStep = 0;
+  let pos = 0;
+
+  let timeout: NodeJS.Timeout | null = null;
+
+  const tick = () => {
+    console.log("Starting TO for", steps[pos]);
+    timeout = setTimeout(() => {
+      console.log("STO", steps[pos]);
+      fn();
+
+      triesAtStep = triesAtStep + 1;
+
+      if (triesAtStep > 5) {
+        triesAtStep = 0;
+        pos = pos >= 2 ? 0 : pos + 1;
+      }
+
+      tick();
+    }, steps[pos]);
+  };
+
+  tick();
+
+  return function cancel() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+  };
+}
+
 export default async function createIntervalHost(config: InternalConfig) {
   console.log("Create Interval Host :)", config);
-  const ws = new ISocket(new WebSocket("ws://localhost:2023"));
 
-  await ws.connect();
+  async function setup() {
+    const ws = new ISocket(new WebSocket("ws://localhost:2023"));
 
-  const caller = createCaller({
-    schema: internalRpcSchema,
-    send: (rawInput) => ws.send(rawInput),
-  });
-  ws.on("message", (m) => caller.replyHandler(m));
+    ws.on("close", () => {
+      console.log("Closed");
+      // auto retry connect here?
+    });
 
-  const loggedIn = await caller.client("LOGIN", { apiKey: config.apiKey });
-  if (!loggedIn) throw new Error("The provided API key is not valid");
+    const caller = createCaller({
+      schema: internalRpcSchema,
+      send: (rawInput) => ws.send(rawInput),
+    });
+    ws.on("message", (m) => caller.replyHandler(m));
+
+    await ws.connect();
+    console.log("Connected!");
+
+    const loggedIn = await caller.client("LOGIN", { apiKey: config.apiKey });
+    if (!loggedIn) throw new Error("The provided API key is not valid");
+  }
+
+  setup();
 
   return true;
 }
