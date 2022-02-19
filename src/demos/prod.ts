d472c0c77c15ecd9adf5e940670d959f084aeb5a
Dan Philibin <dan@danphilibin.com>
2022-02-19 14:12:21 -0800

    add prod example API key to seed data

--- src/demos/prod.ts
+++ src/demos/prod.ts
@@ -1,23 +1,26 @@
 import Interval from '../index'
 
 const interval = new Interval({
-  // TODO: update seed data with this API key
   apiKey: 'live_T31EqcPqmzWzZy2AegrE7wxKKvzDS1zqEfH4w6vq5R3o8jUE',
   logLevel: 'debug',
   endpoint: 'ws://localhost:3002',
   actions: {
     enter_two_numbers: async io => {
-      const num = await io.input.number('Enter a number')
+      const num1 = await io.input.number('Enter a number')
 
-      await io.input.number(
-        `Enter a second number that's greater than ${num}`,
+      const num2 = await io.input.number(
+        `Enter a second number that's greater than ${num1}`,
         {
-          min: num + 1,
+          min: num1 + 1,
         }
       )
+
+      return { num1, num2 }
     },
     enter_one_number: async io => {
       const num = await io.input.number('Enter a number')
+
+      return { num }
     },
   },
 })
