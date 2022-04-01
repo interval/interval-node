ef52dd9767a555ab1f9a3f7790baaacd2dde7a4d
Jacob Mischka <jacob@mischka.me>
2022-04-01 10:42:00 -0500

    Revert previous commit for selectSingle components
    It breaks them, somehow.

--- src/components/selectSingle.ts
+++ src/components/selectSingle.ts
@@ -2,11 +2,16 @@ import component from '../component'
 import type { T_IO_PROPS } from '../ioSchema'
 import type { IOPromiseConstructor, IOPromise } from '../io'
 
-export function selectSingle<
-  Props extends T_IO_PROPS<'SELECT_SINGLE'>,
-  Options extends Props['options']
->(constructor: IOPromiseConstructor<'SELECT_SINGLE', Options[0]>) {
-  return (label: string, props: Props) => {
+export function selectSingle(
+  constructor: IOPromiseConstructor<'SELECT_SINGLE'>
+) {
+  return <
+    Props extends T_IO_PROPS<'SELECT_SINGLE'>,
+    Options extends Props['options']
+  >(
+    label: string,
+    props: Props
+  ) => {
     return constructor(component('SELECT_SINGLE', label, props)) as IOPromise<
       'SELECT_SINGLE',
       Options[0]
@@ -14,15 +19,20 @@ export function selectSingle<
   }
 }
 
-export default function findAndSelect<
-  Props extends Omit<T_IO_PROPS<'SELECT_SINGLE'>, 'options'> & {
-    initialOptions?: Options
-    onSearch: (query: string) => Promise<OptionsLike>
-  },
-  OptionsLike extends T_IO_PROPS<'SELECT_SINGLE'>['options'],
-  Options extends Awaited<ReturnType<Props['onSearch']>>
->(constructor: IOPromiseConstructor<'SELECT_SINGLE', Options[0]>) {
-  return (label: string, props: Props) => {
+export default function findAndSelect(
+  constructor: IOPromiseConstructor<'SELECT_SINGLE'>
+) {
+  return <
+    Props extends Omit<T_IO_PROPS<'SELECT_SINGLE'>, 'options'> & {
+      initialOptions?: Options
+      onSearch: (query: string) => Promise<OptionsLike>
+    },
+    OptionsLike extends T_IO_PROPS<'SELECT_SINGLE'>['options'],
+    Options extends Awaited<ReturnType<Props['onSearch']>>
+  >(
+    label: string,
+    props: Props
+  ) => {
     const { onSearch, initialOptions, ...rest } = props
     const c = component(
       'SELECT_SINGLE',
