8eeb99a7818d1465d96790c2d550a7f6fa65034b
Jacob Mischka <jacob@mischka.me>
2022-04-01 09:56:18 -0500

    Specify return type argument in IOPromiseConstructor
    I'm not sure why this is necessary for SELECT_MULTIPLE but many other
    components don't need it. The `.optional()` return type was right before
    even in a group, just not the required one. Strange.
    
    Closes #361

--- src/components/selectSingle.ts
+++ src/components/selectSingle.ts
@@ -2,16 +2,11 @@ import component from '../component'
 import type { T_IO_PROPS } from '../ioSchema'
 import type { IOPromiseConstructor, IOPromise } from '../io'
 
-export function selectSingle(
-  constructor: IOPromiseConstructor<'SELECT_SINGLE'>
-) {
-  return <
-    Props extends T_IO_PROPS<'SELECT_SINGLE'>,
-    Options extends Props['options']
-  >(
-    label: string,
-    props: Props
-  ) => {
+export function selectSingle<
+  Props extends T_IO_PROPS<'SELECT_SINGLE'>,
+  Options extends Props['options']
+>(constructor: IOPromiseConstructor<'SELECT_SINGLE', Options[0]>) {
+  return (label: string, props: Props) => {
     return constructor(component('SELECT_SINGLE', label, props)) as IOPromise<
       'SELECT_SINGLE',
       Options[0]
@@ -19,20 +14,15 @@ export function selectSingle(
   }
 }
 
-export default function findAndSelect(
-  constructor: IOPromiseConstructor<'SELECT_SINGLE'>
-) {
-  return <
-    Props extends Omit<T_IO_PROPS<'SELECT_SINGLE'>, 'options'> & {
-      initialOptions?: Options
-      onSearch: (query: string) => Promise<OptionsLike>
-    },
-    OptionsLike extends T_IO_PROPS<'SELECT_SINGLE'>['options'],
-    Options extends Awaited<ReturnType<Props['onSearch']>>
-  >(
-    label: string,
-    props: Props
-  ) => {
+export default function findAndSelect<
+  Props extends Omit<T_IO_PROPS<'SELECT_SINGLE'>, 'options'> & {
+    initialOptions?: Options
+    onSearch: (query: string) => Promise<OptionsLike>
+  },
+  OptionsLike extends T_IO_PROPS<'SELECT_SINGLE'>['options'],
+  Options extends Awaited<ReturnType<Props['onSearch']>>
+>(constructor: IOPromiseConstructor<'SELECT_SINGLE', Options[0]>) {
+  return (label: string, props: Props) => {
     const { onSearch, initialOptions, ...rest } = props
     const c = component(
       'SELECT_SINGLE',
