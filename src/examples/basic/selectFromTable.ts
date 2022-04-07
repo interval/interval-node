1071be1a0938a2047433c1e095b6a9af947c1510
Jacob Mischka <jacob@mischka.me>
2022-04-07 09:15:23 -0500

    Fix table test for formatted data

--- src/examples/basic/selectFromTable.ts
+++ src/examples/basic/selectFromTable.ts
@@ -5,12 +5,14 @@ const charges = [
     id: 'b717a9cf-4a3e-41ab-bcda-e2f3ff35c974',
     name: 'Alex',
     amount: 15000,
+    purchasedAt: new Date(),
   },
   {
     id: 'acc14b04-60d8-4f9d-9907-10ea1ed05fe2',
     name: 'Dan',
     amount: 0,
     promoCode: 'APPLE',
+    purchasedAt: new Date(),
   },
   {
     id: '91032195-6836-4573-9cd5-0b06ea2379ec',
@@ -18,6 +20,7 @@ const charges = [
     amount: 1200,
     promoCode: 'BANANA',
     arr: [1, 2, 3],
+    purchasedAt: new Date(),
   },
   {
     id: '48d10a1a-9c8c-4426-8d0c-796610c652f3',
@@ -27,6 +30,7 @@ const charges = [
     nested: {
       a: 'b',
     },
+    purchasedAt: new Date(),
   },
 ]
 
@@ -69,9 +73,16 @@ export const table_custom_columns: IntervalActionHandler = async io => {
         }),
       },
       {
-        label: 'Promo Code',
+        label: 'Promo code',
         render: (row: Charge) => ({
           label: row.promoCode,
+        }),
+      },
+      {
+        label: 'Purchased at',
+        render: (row: Charge) => ({
+          label: row.purchasedAt.toLocaleString(),
+          value: row.purchasedAt,
         }),
       },
     ],
