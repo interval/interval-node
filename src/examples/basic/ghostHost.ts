e824ebdf5adb50d877820a5395fb969ad60ad9ff
Alex Arena <arena.secure@gmail.com>
2022-05-04 20:50:14 -0700

    shows off how users are authenticated in a ghost context

--- src/examples/basic/ghostHost.ts
+++ src/examples/basic/ghostHost.ts
@@ -3,9 +3,11 @@ import Interval from '../..'
 const anon = new Interval({
   endpoint: 'ws://localhost:3000/websocket',
   actions: {
-    hello_world: async io => {
-      const name = await io.input.text('Your name')
-      return { greeting: `Hello, ${name}` }
+    hello_world: async (io, ctx) => {
+      const name = await io.input.text('Your name').optional()
+      return {
+        greeting: `Hello, ${name || ctx.user.firstName || ctx.user.email}`,
+      }
     },
   },
 }).listen()
