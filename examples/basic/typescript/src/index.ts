bcba8a4c9bd815d834e0669ca6d1f9286ffc724f
Alex Arena <arena.secure@gmail.com>
2022-06-04 17:01:07 -0700

    changes example action to hello_world

--- examples/basic/typescript/src/index.ts
+++ examples/basic/typescript/src/index.ts
@@ -1,27 +1,12 @@
-import Interval from '@interval/sdk';
+import Interval, { io } from '@interval/sdk';
 import 'dotenv/config'; // loads environment variables from .env
 
 const interval = new Interval({
   apiKey: process.env.INTERVAL_KEY,
   actions: {
-    enter_one_number: async io => {
-      const num = await io.input.number('Enter a number');
-      return {
-        num,
-      };
-    },
-    enter_two_numbers: async io => {
-      const first = await io.input.number('Enter a number');
-      const second = await io.input.number(
-        `Enter a number greater than ${first}`,
-        {
-          min: first + 1,
-        }
-      );
-      return {
-        first,
-        second,
-      };
+    hello_world: async () => {
+      const name = await io.input.text('Enter your name');
+      return `Hello, ${name}!`;
     },
   },
 });
