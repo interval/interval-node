7029294b79053e46fb19289d4da6d4c196cb3331
Jacob Mischka <jacob@mischka.me>
2022-06-21 13:10:18 -0500

    Move persistent connection state into new IntervalClient class
    This allows a lighter Interval class to exist as a user-facing frontend
    for the IntervalClient class, along with the stateless HTTP API
    functions that don't require a persistent connection to Interval.
    
    This is probably for the best in some form regardless, the Interval
    class was getting too big for its own good and had a large mix of
    public and private internal state and functionality. Most of the private
    internal state is now encapsulated into IntervalClient.
    
    There's a little bit of duplication that should be removed in a future
    slight rearchitecture (Interval.close just calling IntervalClient.close,
    etc), but it's not too unwieldy.

--- src/examples/http/index.ts
+++ src/examples/http/index.ts
@@ -1,6 +1,7 @@
-import { createHttpServer } from '../../index'
+import http from 'http'
+import Interval from '../../index'
 
-const server = createHttpServer({
+const interval = new Interval({
   apiKey: 'live_N47qd1BrOMApNPmVd0BiDZQRLkocfdJKzvt8W6JT5ICemrAN',
   logLevel: 'debug',
   endpoint: 'ws://localhost:3000/websocket',
@@ -12,6 +13,8 @@ const server = createHttpServer({
 })
 
 const port = process.env.PORT ? Number(process.env.PORT) : 5000
+
+const server = http.createServer(interval.httpRequestHandler)
 
 server.listen(port)
 
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,56 +1,8 @@
-import http from 'http'
-import { InternalConfig, Interval } from '..'
+import { IncomingMessage } from 'http'
 
-export async function handleRequest(requestId: string, config: InternalConfig) {
-  const interval = new Interval(config)
-  const response = await interval.respondToRequest(requestId)
-  interval.close()
-
-  return response
-}
-
-export function createHttpServer(config: InternalConfig): http.Server {
-  return http.createServer(createHttpRequestHandler(config))
-}
-
-export function createHttpRequestHandler(config: InternalConfig) {
-  return async (req: http.IncomingMessage, res: http.ServerResponse) => {
-    // TODO: Proper headers
-
-    if (req.method === 'GET') {
-      return res.writeHead(200).end('OK')
-    }
-
-    if (req.method !== 'POST') {
-      return res.writeHead(405).end()
-    }
-
-    try {
-      const body = await getRequestBody(req)
-      if (!body || typeof body !== 'object' || Array.isArray(body)) {
-        return res.writeHead(400).end()
-      }
-
-      const { requestId, httpHostId } = body
-
-      if (requestId) {
-        await handleRequest(requestId, config)
-        return res.writeHead(200).end()
-      } else if (httpHostId) {
-        const interval = new Interval(config)
-        await interval.declareHost(httpHostId)
-        return res.writeHead(200).end()
-      } else {
-        return res.writeHead(400).end()
-      }
-    } catch (err) {
-      console.error('Error in HTTP request handler:', err)
-      return res.writeHead(500).end()
-    }
-  }
-}
-
-async function getRequestBody(req: http.IncomingMessage): Promise<any | null> {
+export async function getRequestBody(
+  req: IncomingMessage
+): Promise<any | null> {
   return new Promise((resolve, reject) => {
     const chunks: string[] = []
 
