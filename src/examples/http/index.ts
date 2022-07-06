b020e04ddf47333f93ecd7f327f53b4665c8b5f1
Jacob Mischka <jacob@mischka.me>
2022-07-06 11:49:03 -0500

    Fix http/serverless example to use new ExperimentalInterval import

--- src/examples/http/index.ts
+++ src/examples/http/index.ts
@@ -1,5 +1,5 @@
 import http from 'http'
-import Interval from '../..'
+import Interval from '../../experimental'
 
 const interval = new Interval({
   apiKey: 'live_N47qd1BrOMApNPmVd0BiDZQRLkocfdJKzvt8W6JT5ICemrAN',
