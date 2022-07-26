1e64859565b5f076611015ef1d732247b3b5ba98
Dan Philibin <dan@danphilibin.com>
2022-07-25 21:44:17 -0700

    fix SDK build without AWS credentials

--- src/examples/utils/upload.ts
+++ src/examples/utils/upload.ts
@@ -7,15 +7,19 @@ import {
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
-export const s3Client = new S3Client({
-  region: AWS_REGION ?? 'us-west-1',
-  credentials: {
-    accessKeyId: AWS_KEY_ID,
-    secretAccessKey: AWS_KEY_SECRET,
-  },
-})
-
 export async function generateUploadUrl(key: string) {
+  if (!AWS_KEY_ID || !AWS_KEY_SECRET || !AWS_S3_IO_BUCKET) {
+    throw new Error('Missing AWS credentials')
+  }
+
+  const s3Client = new S3Client({
+    region: AWS_REGION ?? 'us-west-1',
+    credentials: {
+      accessKeyId: AWS_KEY_ID,
+      secretAccessKey: AWS_KEY_SECRET,
+    },
+  })
+
   const command = new PutObjectCommand({
     Bucket: AWS_S3_IO_BUCKET,
     Key: key,
@@ -29,5 +33,9 @@ export async function generateUploadUrl(key: string) {
 }
 
 export function generateDownloadUrl(path: string) {
+  if (!AWS_S3_IO_BUCKET || !AWS_REGION) {
+    throw new Error('Missing AWS credentials')
+  }
+
   return `https://${AWS_S3_IO_BUCKET}.s3.${AWS_REGION}.amazonaws.com/${path}`
 }
