b2ae246e5b7f91016fd8191ab83b756d30db4b87
Ryan Coppolo <r@coppolo.com>
2022-07-28 12:28:26 -0400

    Add all the other inputs and update example action

--- src/examples/basic/index.ts
+++ src/examples/basic/index.ts
@@ -128,7 +128,70 @@ const interval = new Interval({
   endpoint: 'ws://localhost:3000/websocket',
   actions: {
     disabled_inputs: async io => {
-      await io.input.text('Text you can’t edit').disabled(true)
+      // TODO
+      // should we make this work?
+      // await io.confirm("Can't edit this").disabled(true)
+
+      await io.group([
+        io.display.heading('This is a bunch of disabled inputs'),
+        io.input.text('Text input').disabled(true),
+        io.input.boolean('Boolean input').disabled(true),
+        io.input.number('Number input').disabled(true),
+        io.input.email('Email input').disabled(true),
+        io.input.richText('Rich text input').disabled(true),
+        io
+          .search('Search for a user', {
+            renderResult: user => ({
+              label: user.name,
+              description: user.email,
+            }),
+            onSearch: async query => {
+              return [
+                {
+                  name: 'John Doe',
+                  email: 'johndoe@example.com',
+                },
+              ]
+            },
+          })
+          .disabled(true),
+        io.select
+          .single('Select something', {
+            options: [1, 2, 3],
+          })
+          .disabled(true),
+        io.select
+          .multiple('Select multiple of something', {
+            options: [1, 2, 3],
+          })
+          .disabled(true),
+        io.select
+          .table('Select from table', {
+            data: [
+              {
+                album: 'Exile on Main Street',
+                artist: 'The Rolling Stones',
+                year: 1972,
+              },
+              {
+                artist: 'Michael Jackson',
+                album: 'Thriller',
+                year: 1982,
+              },
+              {
+                album: 'Enter the Wu-Tang (36 Chambers)',
+                artist: 'Wu-Tang Clan',
+                year: 1993,
+              },
+            ],
+          })
+          .disabled(true),
+        io.experimental.date('Date input').disabled(true),
+        io.experimental.time('Time input').disabled(true),
+        io.experimental.datetime('Date & time').disabled(true),
+        io.experimental.input.file('File input').disabled(true),
+      ])
+
       return 'Done!'
     },
     'long-return-string': async io => {
