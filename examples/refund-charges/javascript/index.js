5d56c777e6916c00af5afa52ac28f128072c9fc2
Dan Philibin <dan@danphilibin.com>
2022-08-16 21:47:48 +0000

    update JS template in refund-charges
    not sure where this code went but I don't see it in the history anywhere. like the previous commit, I don't think it was an issue until we updated the examples earlier today.

---
+++ examples/refund-charges/javascript/index.js
@@ -0,0 +1,40 @@
+const { Interval, io, ctx } = require('@interval/sdk');
+require('dotenv').config(); // loads environment variables from .env
+const { getCharges, refundCharge } = require('./payments');
+
+const interval = new Interval({
+  apiKey: process.env.INTERVAL_KEY,
+  actions: {
+    refund_user: async () => {
+      const customerEmail = await io.input.email(
+        'Email of the customer to refund:'
+      );
+      console.log('Email:', customerEmail);
+      const charges = await getCharges(customerEmail);
+
+      const chargesToRefund = await io.select.table(
+        'Select one or more charges to refund',
+        {
+          data: charges,
+        }
+      );
+
+      await ctx.loading.start({
+        title: 'Refunding charges',
+        // Because we specified `itemsInQueue`, Interval will render a progress bar versus an indeterminate loading indicator.
+        itemsInQueue: chargesToRefund.length,
+      });
+
+      for (const charge of chargesToRefund) {
+        await refundCharge(charge.id);
+        await ctx.loading.completeOne();
+      }
+
+      // Values returned from actions are automatically stored with Interval transaction logs
+      return { chargesRefunded: chargesToRefund.length };
+    },
+  },
+});
+
+// Establishes a persistent connection between Interval and your app.
+interval.listen();
