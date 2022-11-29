d279e420e61c941d57947e5f8823796815d8f2c4
Dan Philibin <dan@danphilibin.com>
2022-11-29 05:55:18 +0000

    fix the font size in markdown and loading indicators

--- src/examples/basic/index.ts
+++ src/examples/basic/index.ts
@@ -879,10 +879,53 @@ const interval = new Interval({
     },
     Render_markdown: async io => {
       await io.group([
+        // contents taken from tailwind typography demo
         io.display.markdown(`
-          ## User data deletion
-          **Warning:** this _will_ erase user data.
-          You can read more about this [here](https://google.com).
+          # What to expect from here on out
+
+          _This has been adapted from the [Tailwind](https://tailwindcss.com) typography plugin demo._
+        
+          What follows from here is just a bunch of absolute nonsense I've written to demo typography. It includes every sensible typographic element I could think of, like **bold text**, unordered lists, ordered lists, code blocks, block quotes, and _even italics_.
+
+          It's important to cover all of these use cases for a few reasons:
+
+          1. We want everything to look good out of the box.
+          2. Really just the first reason, that's the whole point of the plugin.
+          3. Here's a third pretend reason though a list with three items looks more realistic than a list with two items.
+          
+          Now we're going to try out another header style.
+
+          ## Typography should be easy
+
+          So that's a header for you — with any luck if we've done our job correctly that will look pretty reasonable.
+
+          Something a wise person once told me about typography is:
+
+          > Typography is pretty important if you don't want your stuff to look like trash. Make it good then it won't be bad.
+
+          Now I'm going to show you an example of an unordered list to make sure that looks good, too:
+
+          - Here is the first item in this list.
+          - Let's try longer, more complex list items:
+            - This is a sub-list-item.
+          - And this is the last item in the list.
+
+          ### What does code look like?
+          
+          Code blocks should look okay by default, although most people will probably want to use \`io.display.code\`:
+
+          \`\`\`
+          new Action({
+            name: 'Render markdown',
+            handler: async () => {
+              // ...
+            }
+          })
+          \`\`\`
+          
+          #### And finally, an H4
+
+          And that's the end of this demo.
         `),
         io.select.multiple('Erase user data', {
           options: [
