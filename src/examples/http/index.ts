d9fb5ad48d229ac9449cc9d3f7985c8faf004370
Jacob Mischka <jacob@mischka.me>
2023-02-02 15:00:21 -0600

    Add basic slow cold-start simulation with sleep in http example

--- src/examples/http/index.ts
+++ src/examples/http/index.ts
@@ -2,6 +2,9 @@ import http from 'http'
 import Interval, { Page, Layout, io } from '../../experimental'
 import { asyncTable } from '../utils/ioMethodWrappers'
 
+const sleep = async (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
+
 const interval = new Interval({
   apiKey: 'live_N47qd1BrOMApNPmVd0BiDZQRLkocfdJKzvt8W6JT5ICemrAN',
   logLevel: 'debug',
@@ -30,7 +33,12 @@ const interval = new Interval({
 
 const port = process.env.PORT ? Number(process.env.PORT) : 5000
 
-const server = http.createServer(interval.httpRequestHandler)
+const server = http.createServer(async (req, res) => {
+  // Simulate a slow cold-start time for a serverless function
+  await sleep(2000)
+
+  return interval.httpRequestHandler(req, res)
+})
 
 server.listen(port)
 
