ef356299e21d4ea68ccb9a35217343990197f362
Dan Philibin <dan@danphilibin.com>
2023-05-26 15:55:30 +0000

    add more helpful error notice when credentials is used on web

--- src/examples/basic/index.ts
+++ src/examples/basic/index.ts
@@ -2160,6 +2160,9 @@ const interval = new Interval({
     }),
     grids: gridsPage,
     confirm_identity: confirmIdentity,
+    credentials: async () => {
+      const creds = await io.experimental.credentials('github')
+    },
   },
 })
 
